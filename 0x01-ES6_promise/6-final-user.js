fee59284eb39eed1139744a74f18a4e7eaa3c9ef
Realyoung1 <122838973+Realyoung1@users.noreply.github.com>
2024-01-02 09:49:09 -0800

    Update 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,6 @@
 // this funcs Handles all multiple promises
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
   return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
